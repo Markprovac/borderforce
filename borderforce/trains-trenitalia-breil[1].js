4d2da365367095a53108ab8e8406c6e8a506d082
Markprovac <mark.provac@gmail.com>
2026-07-15 22:18:06 +0200

    Add files via upload

---
+++ borderforce/trains-trenitalia-breil[1].js
@@ -0,0 +1,84 @@
+window.BF_TRENITALIA_BREIL_DATA = {
+  "schemaVersion": 1,
+  "version": "2026-07-15",
+  "gare": "Breil-sur-Roya",
+  "operateur": "Trenitalia",
+  "typeDonnees": "horaires_theoriques",
+  "validiteDu": "2026-06-14",
+  "validiteAu": "2026-12-12",
+  "source": {
+    "nom": "Région Ligurie — service planifié Trenitalia",
+    "note": "Horaires théoriques. Vérifier les perturbations et modifications le jour du service."
+  },
+  "circulationParDefaut": {
+    "joursSemaine": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+    ],
+    "joursFeries": true,
+    "libelle": "Tous les jours, dimanches et jours fériés compris"
+  },
+  "trains": [
+    {
+      "numero": "22952",
+      "origine": "Vintimille",
+      "destination": "Cuneo",
+      "arriveeBreil": "06:55",
+      "departBreil": "06:56"
+    },
+    {
+      "numero": "22955",
+      "origine": "Cuneo",
+      "destination": "Vintimille",
+      "arriveeBreil": "08:41",
+      "departBreil": "08:42"
+    },
+    {
+      "numero": "22953",
+      "origine": "Cuneo",
+      "destination": "Vintimille",
+      "arriveeBreil": "10:36",
+      "departBreil": "10:37"
+    },
+    {
+      "numero": "22956",
+      "origine": "Vintimille",
+      "destination": "Cuneo",
+      "arriveeBreil": "11:17",
+      "departBreil": "11:19"
+    },
+    {
+      "numero": "22959",
+      "origine": "Cuneo",
+      "destination": "Vintimille",
+      "arriveeBreil": "16:43",
+      "departBreil": "16:57"
+    },
+    {
+      "numero": "22958",
+      "origine": "Vintimille",
+      "destination": "Cuneo",
+      "arriveeBreil": "16:55",
+      "departBreil": "17:00"
+    },
+    {
+      "numero": "22964",
+      "origine": "Vintimille",
+      "destination": "Cuneo",
+      "arriveeBreil": "19:18",
+      "departBreil": "19:26"
+    },
+    {
+      "numero": "22957",
+      "origine": "Cuneo",
+      "destination": "Vintimille",
+      "arriveeBreil": "19:24",
+      "departBreil": "19:25"
+    }
+  ]
+};
